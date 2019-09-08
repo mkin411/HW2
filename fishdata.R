3f05520af25f1488dbe330129100ce7425f5d18b
mkin411 <marguerite.kinsella1@louisiana.edu>
2019-09-08 17:36:45 -0500

    fish homework

--- fishdata.R
+++ fishdata.R
@@ -1,1 +1,12 @@
 #fishdata
+nrow ("fish_data. (6)")
+NROW()
+f
+NROW(`fish_data.(6)`)
+NCOL(`fish_data.(6)`)
+typeof(`fish_data.(6)`)
+View(`fish_data.(6)`)
+table (`fish_data.(6)`)
+df[,c(1)]
+df[, 1]
+str(`fish_data.(6)`)
