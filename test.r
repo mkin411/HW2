ed29d7d966cc0dc9ecdd7173eb5c8f66f10e2dfa
mkin411 <marguerite.kinsella1@louisiana.edu>
2019-09-08 17:02:48 -0500

    adding lines

--- test.r
+++ test.r
@@ -2,4 +2,66 @@
 weight_kg <- k
 weight_kg <-k
 weight_kg <- 55
-#averquepasa
+z -> 10
+z ->10
+10 -> z
+5 -> b
+c(0,1,2,3,4)
+c -> [0,1,2,3,4]
+c -> {0,1,3,4}
+c -> c(0,1,2,4)
+c(0,1,2,3,4) <- c
+c -> "hat"
+c -> "this is amazing"
+k ->3L
+3L <- k
+k -> 2L
+2L -> K
+t -> happy
+v->15.5
+v -> 15.5
+V -> 16
+V = 16
+a <- c(1,2, 3, 4)
+"this is amazing"
+1/2
+(20/10)
+factor(1/2)
+a %% K
+a %/% K
+a ^ K
+a %/% K
+a ^ K
+a %/% K
+a ^ K
+d -> 9:25
+d = 9:25
+t = 9:18
+{p = 8; m= 7; g=5}
+p*m+g
+p*(m+g)
+2^1025
+-2^5067
+2^1025
+-2^5062
+is.na(t)
+r <- NA
+is.na(r)
+l <- N
+oi <- c(1, 2, 3, NaN, 5)
+j <- c(5, 6, 7, NULL)
+c <-NULL
+u <- c(9, 11, 12)
+y <- c(13, 14, 15)
+print (u^y)
+20%%2
+x <- c(2, 3, 4, NULL)
+e <- (2, 3, 4, NULL)
+get(m, env = x, inherits = FALSE)
+as.null(2, 3, 4, NULL)
+x[4]
+as.null(2, 3, 4, NULL)
+df <- 1:10
+3%/%4
+10%/%5
+3^4
