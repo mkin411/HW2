be84f9ed5d77aef2c7b3b286a504422892fb8268
mkin411 <marguerite.kinsella1@louisiana.edu>
2019-09-09 15:24:29 -0500

    utd

--- sept9.R
+++ sept9.R
@@ -5,3 +5,9 @@ b <- list(b)
 m <- matrix (data =1:12 ,nrow =4, ncol= 3,
              dimnames = list (c("r1", "r2", "r3", "r4"), 
                               c ("c1", "c2", "c3")))
+m
+#showed columns and rows
+ray <- array (data = 1:24, dim=c(3,4,2))
+#always rows columns, dimensions
+ray[2,3,2]
+ray [3,4,1]
