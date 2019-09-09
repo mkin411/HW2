5c7532a0adaf2d6c849f82a5769d7b216f838ce3
mkin411 <marguerite.kinsella1@louisiana.edu>
2019-09-09 15:51:16 -0500

    added

--- sept9.R
+++ sept9.R
@@ -14,3 +14,17 @@ ray [3,4,1]
 #single brackets returns objects from obj x- double bracket allows for partial matches
 data <- data[data$group == [group]], ]
 #group assigned to something in a list, and shows how it could be pulled out, matrixes refer to position, not used most
+df <- data.frame (a=c(1,2,3,4), b=c(1,2,3,4))
+df.matrix <-as.matrix(df)
+df.matrix
+df
+df[3,2]
+df [3, c("b")]
+df3$b
+df$b
+#pulled out all the values for b, looking for column heading not position
+df [,2]
+#another way to pull out data structures
+f <-parecel.l [parcel.l $group =="fish,"]$transect.id6
+#which data frame, data frame name, $ column and then the information isolating, then just the transect ids where fish were present, 6 shows the value there
+#unique will return unique values in data structure, subsetting has worked, use $ again to pull out another field
