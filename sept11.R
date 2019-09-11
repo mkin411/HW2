9b60a5d92c5ec6af3ddb434a1c1f0db4c6977165
mkin411 <marguerite.kinsella1@louisiana.edu>
2019-09-11 13:53:54 -0500

    notes up

--- sept11.R
+++ sept11.R
@@ -1,1 +1,11 @@
 #sept.11.2019
+now <- Sys.time()
+now
+class(now)
+unclass(now)
+#how many seconds have passed since then, HoPR pg. 49
+mil <-1000000
+mil
+class(mil) <-c("POSIXct", "POSIXt")
+mil
+#ended on page 50 of HoPR
