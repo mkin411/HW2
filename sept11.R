4eaf4ce2810d217fd26f24e5c07a32f39969fe84
mkin411 <marguerite.kinsella1@louisiana.edu>
2019-09-14 14:45:24 -0500

    updated

--- sept11.R
+++ sept11.R
@@ -210,3 +210,48 @@ dim(die5)
 die5
 dim(die5)<-c(1,2,3)
 die5
+mm<-matrix(die5, nrow=2, ncol=3)
+mm
+mm<- matrix(die, nrow = 2, byrow = TRUE)
+mm
+ly <-array(c(1:12, 13:24,25:36),dim=c(3,4,3))
+ly
+hand1 <- c("ace", "king", "queen", "jack", "ten", "spades", "spades", 
+           "spades", "spades", "spades")
+matrix(hand1,nrow=5)
+matrix(hand1,nrow=5, ncol=2)
+dim(hand1)<-c(5,2)
+hand1
+gender <- factor(c("male", "female", "female", "male"))
+attributes(gender)
+unclass(gender)
+gender
+df<-data.frame(face=c("ace,","two","six"), suit =c("clubs,", "clubs", "clubs"),value =c(1,2,3))
+df
+df<-data.frame(face=c("ace,","two","six"), suit =c("clubs,", "clubs", "clubs"),value =c(1,2,3), stringsAsFactors = FALSE)
+df
+str(df)
+#the stringAsFactors function allows the listed atomic vector of suits to no longer be processed as a factor but a a character
+deckof<-data.frame(
+  face = c("king", "queen", "jack", "ten", "nine", "eight", "seven", "six",
+           "five", "four", "three", "two", "ace", "king", "queen", "jack", "ten", 
+           "nine", "eight", "seven", "six", "five", "four", "three", "two", "ace", 
+           "king", "queen", "jack", "ten", "nine", "eight", "seven", "six", "five", 
+           "four", "three", "two", "ace", "king", "queen", "jack", "ten", "nine", 
+           "eight", "seven", "six", "five", "four", "three", "two", "ace"),  
+  suit = c("spades", "spades", "spades", "spades", "spades", "spades", 
+           "spades", "spades", "spades", "spades", "spades", "spades", "spades", 
+           "clubs", "clubs", "clubs", "clubs", "clubs", "clubs", "clubs", "clubs", 
+           "clubs", "clubs", "clubs", "clubs", "clubs", "diamonds", "diamonds", 
+           "diamonds", "diamonds", "diamonds", "diamonds", "diamonds", "diamonds", 
+           "diamonds", "diamonds", "diamonds", "diamonds", "diamonds", "hearts", 
+           "hearts", "hearts", "hearts", "hearts", "hearts", "hearts", "hearts", 
+           "hearts", "hearts", "hearts", "hearts", "hearts"), 
+  value = c(13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 13, 12, 11, 10, 9, 8, 
+            7, 6, 5, 4, 3, 2, 1, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 13, 12, 11, 
+            10, 9, 8, 7, 6, 5, 4, 3, 2, 1)
+)
+deckof
+#now working with ch 14 HoPR- R Notations
+deal(deckof)
+head(deckof)
