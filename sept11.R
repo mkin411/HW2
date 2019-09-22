09420ebb2a7eb7705b5b544354ca7f7341f274a1
mkin411 <marguerite.kinsella1@louisiana.edu>
2019-09-22 09:29:59 -0500

    updated

--- sept11.R
+++ sept11.R
@@ -311,3 +311,28 @@ if.then2("b")
 #you need to use parentheses instead of brackets because it pulls out the listed value, brackets are used for arrays
 if.then3<-function(x){if(x =="a") "baby" else if (x =="b") "father" else if (x =="c") "mother" else "brother"}
 if.then3("c")
+av <- 1
+bm <- 1
+if (av>bm) {print("a wins!")} else if (bm>av) {print("b wins!")} else{print("Tie")}
+if (av > bm) {
+  print("A wins!")
+} else if (av < bm) {
+  print("B wins!")
+} else {
+  print("Tie.")
+}
+#dont need the third bm=av, the other option is just to print
+#array vs matrix review
+happy<-matrix(1:9, 3,3)
+happy
+#matrix showing three rows and 3 columns
+a.ray2<-array(c(1:24), dim=c(3,4,2))
+a.ray2
+vector1 <- c(5,9,3)
+vector2 <- c(10,11,12,13,14,15)
+column.names <- c("COL1","COL2","COL3")
+row.names <- c("ROW1","ROW2","ROW3")
+matrix.names <- c("Matrix1","Matrix2")
+data.frame1<-array(c(vector1, vector2), dim= c(3,3,2), dimnames=list(row.names, column.names, matrix.names))
+data.frame1
+#no need to put the rows, columns and martix in comments, as they are already identified
