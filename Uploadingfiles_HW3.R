ce5b458e7aa1ac98c10185f445853a244c7d5c12
mkin411 <marguerite.kinsella1@louisiana.edu>
2019-09-25 13:03:48 -0500

    bioassay file

--- Uploadingfiles_HW3.R
+++ Uploadingfiles_HW3.R
@@ -10,4 +10,37 @@ head(read.xls)
 mydata2= read.xls("Aurelia_SEAMAP_2012-2018_30minCell.xlsx")
 #could't read xlsx file
 #need to download xlsx file as a csv
+mydata3 = read.csv("Aurelia_SEAMAP_2012-2018_30minCell.csv")
+head(mydata3)
+str(mydata3)
+mydata4= read.csv("ENVREC.csv")
+str(mydata4)
+mydata5<-read.csv("CTDREC.csv")
+str(mydata5)
+#loaded three files
+#practice with my own dataset
+bioassay<-read.csv("Bioassay_Day1 - Sheet1.csv")
+str(bioassay)
+bioassay$X.4
+bioassay$Bioassay
+bioassay$X.2
+LabID<-bioassay$X.2
+LabID
+bioassay$X.1
+AnalysisDate<-bioassay$X.1
+AnalysisDate
+bioassay$AnalysisDate
+bioassay$X.3
+PbTx1<-bioassay$X.3
+PbTx1
+bioassay$X.4
+bioassay$X.4->PbTx2
+head(PbTx2)
+bioassay$X.5
+PbTx3<-bioassay$X.5
+PbTx3
+bioassay$X.6
+PbTxCA<-bioassay$X.6
+PbTxCA
+
 
