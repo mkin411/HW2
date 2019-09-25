ec90c2174623ebb699054ec09638b6afd5e3d2ae
mkin411 <marguerite.kinsella1@louisiana.edu>
2019-09-25 09:56:03 -0500

    updated

--- Uploadingfiles_HW3.R
+++ Uploadingfiles_HW3.R
@@ -1,1 +1,13 @@
 #Uploading Files and HW3
+mydata = read.csv("CRUISES.csv")
+head(mydata)
+str(mydata)
+mydata$YR
+#loaded one cvs file
+library(gdata)
+install.packages(gdata)
+head(read.xls)
+mydata2= read.xls("Aurelia_SEAMAP_2012-2018_30minCell.xlsx")
+#could't read xlsx file
+#need to download xlsx file as a csv
+
